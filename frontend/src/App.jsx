aec21014b729b7c7a179363b2c36b36bdd866df1
[sonambhul0310] <[sonam.bhule9696@gmail.com]>
2024-08-31 09:57:20 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
         <div className="container container-fluid">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/" />
           </Routes>
           <Home/>
         </div>
