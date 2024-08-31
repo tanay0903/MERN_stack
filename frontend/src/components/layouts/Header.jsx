73729e283e67873e242062410c900ffe0224dd3b
[sonambhul0310] <[sonam.bhule9696@gmail.com]>
2024-08-31 20:30:46 +0530

    changesssssss

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
     </div>
     {/* Search Bar and Icon */}
     <div className="col-12 col-md-6 mt-2 mt-md-6">
-      <Search/>
+      <Search />
     </div>
     <div className="col-12 col-md-3 mt-4 mt-md-0">
       <span className="ml-3" id="cart">
